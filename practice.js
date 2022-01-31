e3b62115b68bf3998401201fab539388e0cf314e
Sula002 <96619599+Sula002@users.noreply.github.com>
2022-01-31 18:26:19 +0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -12,8 +12,67 @@ function Divide(){
   document.getElementById("res").innerHTML=a/b;
 }
   var now=new Date();
-  document.getElementById("year").innerHTML="Year : 2022";
-  document.getElementById("weekday").innerHTML="Today is : Monday";
-  document.getElementById("day").innerHTML="Date: 31";
-  document.getElementById("month").innerHTML="Month: January";
+  document.getElementById("year").innerHTML="Year : "+now.getFullYear();
+  switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+  document.getElementById("weekday").innerHTML="Today is : "+day;
+  switch (new Date().getMonth()) {
+  case 0:
+    month = "January";
+    break;
+  case 1:
+    month = "February";
+    break;
+  case 2:
+     month = "March";
+    break;
+  case 3:
+    month = "April";
+    break;
+  case 4:
+    month = "May";
+    break;
+  case 5:
+    month = "June";
+    break;
+  case 6:
+    month = "July";
+    break;
+  case 7:
+    month="August";
+    break;
+  case 8:
+    month="September";
+    break;
+  case 9:
+    month="October";
+    break;
+  case 10:
+    month="November";
+    break;
+  case 11:
+    month="December";
+}
+    document.getElementById("month").innerHTML="Date : "+month;
+  document.getElementById("day").innerHTML="Date : "+now.getDate();
   document.getElementById("time").innerHTML="Current time is : "+now.getHours()+" : "+now.getMinutes()+" : "+now.getSeconds();
